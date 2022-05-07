1b8200cb881f7c4c0bb458584dd43fe0c6ab07f4
saraXX <sara99.edu@gmail.com>
2022-05-07 16:22:35 +0300

    Initial commit

---
+++ app/src/main/java/android/guide/testgit3/MainActivity.java
@@ -0,0 +1,14 @@
+package android.guide.testgit3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
