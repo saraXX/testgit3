94aab6c4803151f8d27f9a2825562285bd3d6db8
saraXX <sara99.edu@gmail.com>
2022-05-07 16:25:46 +0300

    hi

--- app/src/main/java/android/guide/testgit3/MainActivity.java
+++ app/src/main/java/android/guide/testgit3/MainActivity.java
@@ -3,6 +3,7 @@ package android.guide.testgit3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("TAG", "onCreate: g");
+        Log.d("TAG", "onCreate: dfasfasd");
+        Log.d("TAG", "onCreate: dfasfasd");
+        Log.d("TAG", "onCreate: argeahtghgntryt");
+        Log.d("TAG", "onCreate: dfasfdsfsdfsdfasd");
+        Log.d("TAG", "onCreate: a");
+
     }
 }
